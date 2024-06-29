c12ddb598875b57a2ce895d3b2d3615f13752a4d
Abdulalim595 <107740715+Abdulalim595@users.noreply.github.com>
2024-06-29 12:28:25 +0600

    some added

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,6 +29,28 @@ $(document).ready(function () {
     },
   });
 
+  $(".category__slider").owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+      1200: {
+        items: 5,
+      },
+    },
+  });
+
   $(".produsele__cards2").owlCarousel({
     items: 1,
     loop: true,
